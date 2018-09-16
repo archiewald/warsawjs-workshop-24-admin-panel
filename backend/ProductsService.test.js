91921cd7c23c1ab6a98fa8f85e155430c50c6aa8
archiewald <artur.kozubek1@gmail.com>
2018-09-16 13:54:40 +0200

    rename

--- backend/ProductsService.test.js
+++ backend/ProductsService.test.js
@@ -1,20 +1,20 @@
-import ProductService from "./ProductService";
+import ProductsService from "./ProductsService";
 import isPromise from "is-promise";
 
 test("ProductService is an object", () => {
-    expect(typeof ProductService).toBe("object");
+    expect(typeof ProductsService).toBe("object");
 })
 
 test("It has a fetchList() function", () => {
-    expect(typeof ProductService.fetchList).toBe("function");
+    expect(typeof ProductsService.fetchList).toBe("function");
 })
 
 test("fetchList returns promise", async () => {
-    const promise = isPromise((ProductService.fetchList()));
+    const promise = isPromise((ProductsService.fetchList()));
     expect(promise).toBeTruthy();
 })
 
 test("fetchList returns array of elements", async () => {
-    const products = await ProductService.fetchList();
+    const products = await ProductsService.fetchList();
     expect(products.length).toBeGreaterThan(0);
 })
