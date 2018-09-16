e49eafd1e1ab4627702a30dc20dce499046a5acf
archiewald <artur.kozubek1@gmail.com>
2018-09-16 12:35:56 +0200

    test fetch function

--- ProductsService.test.js
+++ ProductsService.test.js
@@ -1,5 +1,20 @@
-import ProductService from "./ProductService"
+import ProductService from "./ProductService";
+import isPromise from "is-promise";
 
 test("ProductService is an object", () => {
-    expect(typeof(ProductService)).toBe("object");
+    expect(typeof ProductService).toBe("object");
+})
+
+test("It has a fetchList() function", () => {
+    expect(typeof ProductService.fetchList).toBe("function");
+})
+
+test("fetchList returns promise", async () => {
+    const promise = isPromise((ProductService.fetchList()));
+    expect(promise).toBeTruthy();
+})
+
+test("fetchList returns array of elements", async () => {
+    const products = await ProductService.fetchList();
+    expect(products.length).toBeGreaterThan(0);
 })
